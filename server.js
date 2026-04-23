d7afd98e539c73cf720350093a432659a3e6b68a
tavochivix <tavochivix@gmail.com>
2026-04-23 23:30:50 +0000

    Tavo Tell Prueba Node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3666
 
 app.listen(PORT, () => {
-    console.log('Servidor escuchando en puerto ${PORT}');
+    console.log(`Servidor escuchando en puerto ${PORT}`);
 });
