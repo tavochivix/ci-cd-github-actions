99ba8390c92431ba6b595363ef335bcff5f1d619
tavochivix <tavochivix@gmail.com>
2026-04-23 23:40:55 +0000

    Ajuste en test

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", ()=> {
     it("debe responder con 200 y un mensaje", async() => {
         const res = await request(app).get("/");
         expect(res.statusCode).toBe(200);
-        expect(res.body.message).toBe("API Funcionando");
+        expect(res.body.message).toBe("Tamo activo");
     });
 });
